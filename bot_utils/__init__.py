ce96a12576d6e195174312b3e7af42f044946965
James T <github.jtara1@gmail.com>
2019-02-10 16:43:02 -0800

    refactor: mv deprecated funcs & files to deprecated submodule

--- bot_utils/__init__.py
+++ bot_utils/__init__.py
@@ -1,5 +1,5 @@
 from jtara1_util import setup_logger
-from bot_utils.deprecated_click_functions import click, double_click
+from bot_utils.deprecated.deprecated_click_functions import click, double_click
 
 
 logger = setup_logger('bot_utils')
