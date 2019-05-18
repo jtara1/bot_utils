12e13408d2c346dd871a0d4732ceecb51046709a
James T <github.jtara1@gmail.com>
2019-05-17 17:38:57 -0700

    chg: cleanup some stylin

--- bot_utils/picture_in_picture/computer_vision.py
+++ bot_utils/picture_in_picture/computer_vision.py
@@ -31,7 +31,7 @@ class ComputerVision(DebugAbstractClass):
         template = cv2.imread(template_image_path)
         template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
         template = cv2.Canny(template, 50, 200)
-        (tH, tW) = template.shape[:2]
+        (template_height, template_width) = template.shape[:2]
         # cv2.imshow("Template", template)
 
         # load the image, convert it to grayscale, and initialize the
@@ -45,31 +45,31 @@ class ComputerVision(DebugAbstractClass):
             # resize the image according to the scale, and keep track
             # of the ratio of the resizing
             resized = imutils.resize(gray, width=int(gray.shape[1] * scale))
-            r = gray.shape[1] / float(resized.shape[1])
+            resize_ratio = gray.shape[1] / float(resized.shape[1])
 
             # if the resized image is smaller than the template, then break
             # from the loop
-            if resized.shape[0] < tH or resized.shape[1] < tW:
+            if resized.shape[0] < template_height or resized.shape[1] < template_width:
                 break
 
             # detect edges in the resized, grayscale image and apply template
             # matching to find the template in the image
             edged = cv2.Canny(resized, 50, 200)
             result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
-            (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+            (_, max_val, _, max_loc) = cv2.minMaxLoc(result)
 
             # check to see if the iteration should be visualized
             # draw a bounding box around the detected region
             clone = np.dstack([edged, edged, edged])
-            cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
-                          (maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+            cv2.rectangle(clone, (max_loc[0], max_loc[1]),
+                          (max_loc[0] + template_width, max_loc[1] + template_height), (0, 0, 255), 2)
             # cv2.imshow("Visualize", clone)
             # cv2.waitKey(0)
 
             # if we have found a new maximum correlation value, then update
             # the bookkeeping variable
-            if found is None or maxVal > found[0]:
-                found = (maxVal, maxLoc, r)
+            if found is None or max_val > found[0]:
+                found = (max_val, max_loc, resize_ratio)
 
         if self.debug or write_output_image:
             cv2.imwrite('res.png', img_rgb)
@@ -77,9 +77,9 @@ class ComputerVision(DebugAbstractClass):
 
         # unpack the bookkeeping variable and compute the (x, y) coordinates
         # of the bounding box based on the resized ratio
-        (_, maxLoc, r) = found
-        (startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
-        (endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+        (_, max_loc, resize_ratio) = found
+        (startX, startY) = (int(max_loc[0] * resize_ratio), int(max_loc[1] * resize_ratio))
+        (endX, endY) = (int((max_loc[0] + template_width) * resize_ratio), int((max_loc[1] + template_height) * resize_ratio))
 
         # draw a bounding box around the detected result and display the image
         cv2.rectangle(img_rgb, (startX, startY), (endX, endY), (0, 0, 255), 2)
